80fa6ce68ea37c57e90d8bef6ced018c0b14cd66
Nehal <shahnehal0407@gmail.com>
2022-04-20 11:27:52 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -172,3 +172,9 @@ MESSAGE_TAGS = {
 }
 
 
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hinashah0407@gmail.com'
+EMAIL_HOST_PASSWORD = '2001nehal'
+EMAIL_USE_TLS = 'True'
